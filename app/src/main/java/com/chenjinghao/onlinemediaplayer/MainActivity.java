249241a6b9a0c0b15b269a3e077a27f80a0621ea
flyGeneral <chenjinghao@chenjinghaodeMacBook-Pro.local>
2018-06-24 22:34:22 +0800

    [onlineProgram] add listview about online program
    增加在线直播列表，数据先本地保存

--- app/src/main/java/com/chenjinghao/onlinemediaplayer/MainActivity.java
+++ app/src/main/java/com/chenjinghao/onlinemediaplayer/MainActivity.java
@@ -2,12 +2,20 @@ package com.chenjinghao.onlinemediaplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import onlineProgram.ItemProgramAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView mProgramListViews;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mProgramListViews = findViewById(R.id.lv_online_program_list);
+        mProgramListViews.setAdapter(new ItemProgramAdapter(this));
     }
 }
