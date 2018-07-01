6db417c470d032b68ad69a2f5006138194445612
flyGeneral <chenjinghao@chenjinghaodeMacBook-Pro.local>
2018-07-01 08:44:52 +0800

    [onlineProgram] import vitamio library and finish play basic online program function

--- app/src/main/java/com/chenjinghao/onlinemediaplayer/onlineProgram/LiveActivity.java
+++ app/src/main/java/com/chenjinghao/onlinemediaplayer/onlineProgram/LiveActivity.java
@@ -1,5 +1,8 @@
 package com.chenjinghao.onlinemediaplayer.onlineProgram;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,16 +13,25 @@ import android.widget.TextView;
 
 import com.chenjinghao.onlinemediaplayer.R;
 
+import io.vov.vitamio.MediaPlayer;
+import io.vov.vitamio.Vitamio;
+import io.vov.vitamio.widget.VideoView;
+
 public class LiveActivity extends AppCompatActivity implements View.OnClickListener {
 
     private final String TAG = "LiveActivity";
     private String mUrl;
     private String mTitle;
 
+    private int RETRY_TIMES = 5;
+    private int mCounts = 0;
+
     private RelativeLayout mRlLoadingLayout;
     private ImageView mBackButton;
     private TextView mProgramTitle;
     private ImageView mPlayButton;
+    private VideoView mVideoView;
+    private RelativeLayout mLoadingLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +44,62 @@ public class LiveActivity extends AppCompatActivity implements View.OnClickListe
         Log.d(TAG, " url = "+ mUrl + "--- title = " + mTitle);
 
         initView();
+        initPlayer();
+    }
+
+    private void initPlayer() {
+        Vitamio.isInitialized(getApplicationContext());
+        mVideoView = findViewById(R.id.vv_surface_view);
+        mVideoView.setVideoURI(Uri.parse(mUrl));
+        mVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                mVideoView.start();
+            }
+        });
+        mVideoView.setOnErrorListener(new MediaPlayer.OnErrorListener() {
+            @Override
+            public boolean onError(MediaPlayer mp, int what, int extra) {
+                if (mCounts > RETRY_TIMES){
+                    new AlertDialog.Builder(LiveActivity.this)
+                            .setMessage(R.string.app_name)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    LiveActivity.this.finish();
+                                }
+                            }).setCancelable(false).show();
+                } else {
+                    mVideoView.stopPlayback();
+                    mVideoView.setVideoURI(Uri.parse(mUrl));
+                }
+                mCounts ++;
+                return false;
+            }
+        });
+        mVideoView.setOnInfoListener(new MediaPlayer.OnInfoListener() {
+            @Override
+            public boolean onInfo(MediaPlayer mp, int what, int extra) {
+                switch (what) {
+                    case MediaPlayer.MEDIA_INFO_BUFFERING_START:
+                        mLoadingLayout.setVisibility(View.VISIBLE);
+                        break;
+                    case MediaPlayer.MEDIA_INFO_BUFFERING_END:
+                    case MediaPlayer.MEDIA_INFO_DOWNLOAD_RATE_CHANGED:
+                    case MediaPlayer.MEDIA_INFO_VIDEO_TRACK_LAGGING:
+                        mLoadingLayout.setVisibility(View.GONE);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
     private void initView() {
         mBackButton = findViewById(R.id.iv_back);
         mProgramTitle = findViewById(R.id.tv_program_title);
         mPlayButton = findViewById(R.id.iv_play);
+        mLoadingLayout = findViewById(R.id.rl_loading_layout);
         mBackButton.setOnClickListener(this);
         mPlayButton.setOnClickListener(this);
     }
